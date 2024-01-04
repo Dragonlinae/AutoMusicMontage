f746582e6912caffb80f36625bbf95ebacd1c68a
Joshua <joshuahsu108@gmail.com>
2024-01-03 21:33:52 -0800

    Add scrolling waveform viewer

--- js/index.js
+++ js/index.js
@@ -1,10 +1,30 @@
 var lowpassFrequency = 1000;
-var generated = false;
+var threshold = 0.7
+const timeslice = 100;
+var isGenerated = false;
+var timePrev = 0;
 
-document.getElementById("outputMusic").ontimeupdate = function () {
-    console.log(document.getElementById("outputMusic").currentTime);
-    document.getElementById("outputMusicCanvas").style.left = 1
-    document.getElementById("outputMusicCanvas2").style.left = 1
+window.onload = function () {
+    const canvasContainer = document.getElementById("outputMusicCanvasContainer");
+    setInterval(function () {
+        if (!isGenerated || document.getElementById("outputMusic").currentTime == timePrev) {
+            return;
+        }
+        console.log(document.getElementById("outputMusic").currentTime);
+        var timeGap = document.getElementById("outputMusic").currentTime - timePrev;
+        timePrev = document.getElementById("outputMusic").currentTime;
+        timeGap = Math.min(Math.abs(timeGap), 0.5);
+        canvasContainer.style.transition = "left " + timeGap + "s linear";
+        canvasContainer.style.left = -document.getElementById("outputMusic").currentTime * 44100 / timeslice + "px";
+    }, 100);
+    // document.getElementById("outputMusic").ontimeupdate = function () {
+    //     console.log(document.getElementById("outputMusic").currentTime);
+    //     var timeGap = document.getElementById("outputMusic").currentTime - timePrev;
+    //     timePrev = document.getElementById("outputMusic").currentTime;
+    //     timeGap = Math.min(Math.abs(timeGap), 0.5);
+    //     canvasContainer.style.transition = "left " + timeGap + "s linear";
+    //     canvasContainer.style.left = -document.getElementById("outputMusic").currentTime * 44100 / timeslice + "px";
+    // }
 }
 
 function lowpassFrequencyUpdate(value) {
@@ -16,7 +36,19 @@ function lowpassFrequencyChange(value) {
     lowpassFrequency = value;
     document.getElementById("lowpassSlider").value = value;
     document.getElementById("lowpassSliderInput").value = value;
-    if (generated) {
+    if (isGenerated) {
+        generate();
+    }
+}
+
+function thresholdUpdate(value) {
+    document.getElementById("thresholdSliderInput").value = value;
+}
+
+function thresholdChange(value) {
+    document.getElementById("thresholdSlider").value = value;
+    document.getElementById("thresholdSliderInput").value = value;
+    if (isGenerated) {
         generate();
     }
 }
@@ -31,16 +63,19 @@ function videoUpload() {
     document.getElementById("outputVideo").src = URL.createObjectURL(file);
 }
 
-function generate() {
+async function generate() {
     const musicFile = document.getElementById("inputMusic").files[0];
     const videoFiles = document.getElementById("inputVideo").files;
 
-    const musicPeaks = getMusicPeaks(musicFile);
-    generated = true;
+    const musicBuffer = await getMusicBuffer(musicFile);
+    const lowpassMusicBuffer = await lowPassFilter(musicBuffer);
+    const waveform = musicBuffer.getChannelData(0);
+    const lowpassWaveform = lowpassMusicBuffer.getChannelData(0);
+    await graphWaveform(waveform, lowpassWaveform);
+    isGenerated = true;
 }
 
-function getMusicPeaks(musicFile) {
-    const waveform = getMusicWaveform(musicFile);
+function getMusicPeaks(waveform) {
 }
 
 function resizeCanvas(length) {
@@ -53,107 +88,116 @@ function resizeCanvas(length) {
     canvas2.width = length;
 }
 
-function getMusicWaveform(musicFile) {
+async function getMusicBuffer(musicFile) {
     const AudioContext = window.AudioContext || window.webkitAudioContext;
     const audioContext = new AudioContext();
     const reader = new FileReader();
-    reader.onload = function (e) {
-        audioContext.decodeAudioData(e.target.result, async function (buffer) {
-            const channelData = buffer.getChannelData(0);
+    return new Promise((resolve) => {
+        reader.onload = function (e) {
+            audioContext.decodeAudioData(e.target.result, async function (buffer) {
+                console.log(buffer);
+                resolve(buffer);
+            });
+            console.log("done");
+        };
+        reader.readAsArrayBuffer(musicFile);
+    });
+}
 
-            var maxPCM = 0;
-            for (let i = 0; i < channelData.length; i++) {
-                if (Math.abs(channelData[i]) > maxPCM) {
-                    maxPCM = Math.abs(channelData[i]);
-                }
+function lowPassFilter(buffer) {
+    const offlineContext = new OfflineAudioContext(1, buffer.length, buffer.sampleRate);
+    const source = offlineContext.createBufferSource();
+    source.buffer = buffer;
+    const filter = offlineContext.createBiquadFilter();
+    filter.type = "lowpass";
+    filter.frequency.value = lowpassFrequency;
+    source.connect(filter);
+    filter.connect(offlineContext.destination);
+    source.start(0);
+    offlineContext.startRendering();
+    return new Promise((resolve) => {
+        offlineContext.oncomplete = function (e) {
+            const buffer = e.renderedBuffer;
+            resolve(buffer);
+        };
+    });
+}
+
+
+async function graphWaveform(waveform, lowpassWaveform) {
+    var maxPCM = 0;
+    for (let i = 0; i < waveform.length; i++) {
+        if (Math.abs(waveform[i]) > maxPCM) {
+            maxPCM = Math.abs(waveform[i]);
+        }
+    }
+    console.log(maxPCM);
+    console.log(waveform.length);
+
+    const canvas = document.getElementById("outputMusicCanvas2");
+    const canvasContext = canvas.getContext("2d");
+
+    console.log(timeslice);
+    await resizeCanvas(Math.floor(waveform.length / timeslice));
+
+    canvasContext.fillStyle = "#000000";
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < canvas.width; i++) {
+        const x = i;
+        var ylow = 0;
+        var yhigh = 0;
+        for (let j = 0; j < timeslice; j++) {
+            const y = (waveform[Math.round(i * timeslice + j)] / maxPCM) * (canvas.height / 2);
+            if (y < ylow) {
+                ylow = y;
             }
-            console.log(maxPCM);
-            console.log(channelData.length);
-
-            const canvas = document.getElementById("outputMusicCanvas2");
-            const canvasContext = canvas.getContext("2d");
-            const timeslice = 100;
-            console.log(timeslice);
-            await resizeCanvas(Math.floor(channelData.length / timeslice));
-
-            canvasContext.fillStyle = "#000000";
-            canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-
-            for (let i = 0; i < canvas.width; i++) {
-                const x = i;
-                var ylow = 0;
-                var yhigh = 0;
-                for (let j = 0; j < timeslice; j++) {
-                    const y = (channelData[Math.round(i * timeslice + j)] / maxPCM) * (canvas.height / 2);
-                    if (y < ylow) {
-                        ylow = y;
-                    }
-                    if (y > yhigh) {
-                        yhigh = y;
-                    }
-                }
-                // if (i > 50000 && i < 50100) {
-                //     console.log(y);
-                // }
-                canvasContext.fillStyle = "#ffffff";
-                canvasContext.fillRect(x, canvas.height / 2 + ylow, 1, yhigh - ylow);
+            if (y > yhigh) {
+                yhigh = y;
             }
+        }
+        // if (i > 50000 && i < 50100) {
+        //     console.log(y);
+        // }
+        canvasContext.fillStyle = "#ffffff";
+        canvasContext.fillRect(x, canvas.height / 2 + ylow, 1, yhigh - ylow);
+    }
 
-            const offlineContext = new OfflineAudioContext(1, buffer.length, buffer.sampleRate);
-            const source = offlineContext.createBufferSource();
-            source.buffer = buffer;
-            const filter = offlineContext.createBiquadFilter();
-            filter.type = "lowpass";
-            filter.frequency.value = lowpassFrequency;
-            source.connect(filter);
-            filter.connect(offlineContext.destination);
-            source.start(0);
-            offlineContext.startRendering();
-            offlineContext.oncomplete = function (e) {
-                const buffer = e.renderedBuffer;
+    const lowpassCanvas = document.getElementById("outputMusicCanvas");
+    const lowpassCanvasContext = lowpassCanvas.getContext("2d");
+    lowpassCanvasContext.fillStyle = "#000000";
+    lowpassCanvasContext.fillRect(0, 0, lowpassCanvas.width, lowpassCanvas.height);
+    var maxPCM = 0;
+    for (let i = 0; i < lowpassWaveform.length; i++) {
+        if (Math.abs(lowpassWaveform[i]) > maxPCM) {
+            maxPCM = Math.abs(lowpassWaveform[i]);
+        }
+    }
+    console.log(maxPCM);
+    console.log(lowpassWaveform.length);
 
-                const channelData = buffer.getChannelData(0);
-                const canvas = document.getElementById("outputMusicCanvas");
-                const canvasContext = canvas.getContext("2d");
-                canvasContext.fillStyle = "#000000";
-                canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-                var maxPCM = 0;
-                for (let i = 0; i < channelData.length; i++) {
-                    if (Math.abs(channelData[i]) > maxPCM) {
-                        maxPCM = Math.abs(channelData[i]);
-                    }
-                }
-                console.log(maxPCM);
-                console.log(channelData.length);
+    console.log(timeslice);
 
-                const timeslice = 100;
-                console.log(timeslice);
-
-                for (let i = 0; i < canvas.width; i++) {
-                    const x = i;
-                    var ylow = 0;
-                    var yhigh = 0;
-                    for (let j = 0; j < timeslice; j++) {
-                        const y = (channelData[Math.round(i * timeslice + j)] / maxPCM) * (canvas.height / 2);
-                        if (y < ylow) {
-                            ylow = y;
-                        }
-                        if (y > yhigh) {
-                            yhigh = y;
-                        }
-                    }
-                    // if (i > 50000 && i < 50100) {
-                    //     console.log(y);
-                    // }
-                    canvasContext.fillStyle = "#ffffff";
-                    canvasContext.fillRect(x, canvas.height / 2 + ylow, 1, yhigh - ylow);
-                }
-                // const peaks = getPeaks([buffer.getChannelData(0), buffer.getChannelData(1)]);
-                // console.log(peaks);
-                // return peaks;
-            };
-        });
-        console.log("done");
-    };
-    reader.readAsArrayBuffer(musicFile);
+    for (let i = 0; i < lowpassCanvas.width; i++) {
+        const x = i;
+        var ylow = 0;
+        var yhigh = 0;
+        for (let j = 0; j < timeslice; j++) {
+            const y = (lowpassWaveform[Math.round(i * timeslice + j)] / maxPCM) * (lowpassCanvas.height / 2);
+            if (y < ylow) {
+                ylow = y;
+            }
+            if (y > yhigh) {
+                yhigh = y;
+            }
+        }
+        // if (i > 50000 && i < 50100) {
+        //     console.log(y);
+        // }
+        lowpassCanvasContext.fillStyle = "#ffffff";
+        lowpassCanvasContext.fillRect(x, lowpassCanvas.height / 2 + ylow, 1, yhigh - ylow);
+    }
+    // const peaks = getPeaks([buffer.getChannelData(0), buffer.getChannelData(1)]);
+    // console.log(peaks);
+    // return peaks;
 }
